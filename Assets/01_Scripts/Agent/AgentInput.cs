f997443650a11a7c2f04ac38251b16e980e4b406
leewnstj <ij35492581@gmail.com>
2023-03-14 17:55:14 +0900

    start

---
+++ Assets/01_Scripts/Agent/AgentInput.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AgentInput : MonoBehaviour, IAgentInput
+{
+    [field : SerializeField] public UnityEvent<Vector2> OnMovementKeyPress { get; set; }
+    [field: SerializeField] public UnityEvent<Vector2> OnPointerPositionChaged { get; set; }
+    [field: SerializeField] public UnityEvent OnFireButtonPress { get; set; }
+    [field: SerializeField] public UnityEvent OnFireButtonRelease { get; set; }
+
+    private void Update()
+    {
+        GetMovementInput();
+        GetPointerInput();
+    }
+
+    private void GetPointerInput()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = 0;
+        Vector2 mouseInWorldPos = Camera.main.ScreenToWorldPoint(mousePos);
+        OnPointerPositionChaged.Invoke(mouseInWorldPos);
+    }
+
+    private void GetMovementInput()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        OnMovementKeyPress?.Invoke(new Vector2(h, v));
+    }
+}
