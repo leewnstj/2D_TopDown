734bd2f0ea5499b6876c43ba0fa7609a429630ca
leewnstj <ij35492581@gmail.com>
2023-03-14 18:37:50 +0900

    Weapon

--- Assets/01_Scripts/Agent/AgentRenderer.cs
+++ Assets/01_Scripts/Agent/AgentRenderer.cs
@@ -4,25 +4,25 @@ using UnityEngine;
 
 public class AgentRenderer : MonoBehaviour
 {
-    private SpriteRenderer _spriteRenderer;
+    private SpriteRenderer _spriteRendere;
 
     private void Awake()
     {
-        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _spriteRendere = GetComponent<SpriteRenderer>();
     }
 
-    public void FaceDirection(Vector3 pointerInput)
+    public void FaceDirection(Vector2 pointerInput)
     {
         Vector3 direction = (Vector3)pointerInput - transform.position;
         Vector3 result = Vector3.Cross(Vector2.up, direction);
 
-        if(result.z > 0)
+        if (result.z > 0)
         {
-            _spriteRenderer.flipX = true;
+            _spriteRendere.flipX = true;
         }
-        else if(result.z < 0)
+        else if (result.z < 0)
         {
-            _spriteRenderer.flipX = true;
+            _spriteRendere.flipX = false;
         }
     }
 }
